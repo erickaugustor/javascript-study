2506df33fb57a44019dcf71a21ab1be9b8d87fd4
erick.augustor <erick.augustoramos@gmail.com>
2017-12-15 14:20:50 -0200

    sum test

---
+++ javascript - study - calculator/index.js
@@ -0,0 +1,8 @@
+function Calcular(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+
+    var res = parseInt(num1)+parseInt(num2);
+
+    alert(res);
+}
