b7f618fbafd1847ffc4f55fdb1147f8bd4fa5ad0
erick.augustor <erick.augustoramos@gmail.com>
2017-12-16 14:33:53 -0200

    let, const, var

--- javascript - study -/index.js
+++ javascript - study -/index.js
@@ -1,16 +1,41 @@
+function getValue(condition){
+    
+    const maxValue = 100;
 
-//strings
-var msg = "Hello Word!";
-console.log(msg.startsWith("Hello"));
-console.log(msg.endsWith("!"));
-console.log(msg.includes("x"));
-console.log(msg.startsWith("Hello", 4));    //retornará true
-//irá retornar falso ou true
-
-
+    if(condition){
+        var myValue="Erick";
+        //com let:
+        //let myValue
 
 
-//listener do click
-document.getElementById("click").addEventListener("click", function(){
-    alert('OI');
-})
+        maxValue = 120;
+        //erro, não se muda valor da const
+
+        const person ={
+            name: 'Erick'
+        }
+
+        person.name = "José";
+
+        person = {
+            name: "José"
+        }
+        //retornará erro
+
+
+        return myValue;
+    }else{
+        console.log(myValue);
+        //por causa do hoisting, é undefined
+        return null;
+    }
+
+}
+
+
+console.log(getValue(false));
+//daria erro com false, pq myValue não está definida
+//está apenas no escopo
+
+
+
