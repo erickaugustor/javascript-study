3e5aeb87457e2d2886f7dd703a67f7c9d8fba455
erickaugustor <erick.augustoramos@gmail.com>
2018-02-09 13:27:22 -0200

    Fundamentals - NodeJS

--- NodeJS/NodeJS Developer Course/1. Fundamentals/4.restarting-app-with-nodemon/app.js
+++ NodeJS/NodeJS Developer Course/1. Fundamentals/4.restarting-app-with-nodemon/app.js
@@ -1,7 +1,9 @@
 //Instal nodemon
 //npm install nodemon -g, para colocar na máquina (-g)
 
+console.log('---------------------');
 console.log('Hello from app.js');
+console.log('---------------------');
 
 const fs = require('fs');
 const _ = require('lodash');
@@ -17,25 +19,74 @@ const notes = require('./notes.js');
 const argv = yargs.argv;
 
 var command = process.argv[2];
-console.log('Command: ', command);
-console.log('Process: ', process.argv);
-console.log('Yargs: ', argv);
+//console.log('Command: ', command);
+//console.log('Process: ', process.argv);
+//console.log('Yargs: ', argv);
+
+
+/*
+    Main
+*/
 
 if(command === 'add'){
+    console.log('---------------------');
     console.log('Adding new note');
-    notes.addNote(argv.title, argv.body);
+    console.log('---------------------');
+
+
+    var note = notes.addNote(argv.title, argv.body);
+    if(note){
+        console.log('---------------------');
+        console.log('Note created.');
+        notes.logNote(note);
+    }else{
+        console.log('---------------------');
+        console.log('Note title taken');
+        console.log('---------------------');
+    }
+
 }else if(command === 'list'){
+    console.log('---------------------');
     console.log('Listing all notes');
+    console.log('---------------------');
     notes.getAll();
+
 }else if(command === 'read'){
+    console.log('---------------------');
     console.log('Reading note');
-    notes.getNotes(argv.title);
+    console.log('---------------------');
+
+    var note = notes.getNotes(argv.title);
+    if(note){
+        console.log('---------------------');
+        console.log('Note found');
+        console.log('---------------------');
+        notes.logNote(note);
+    }else{
+        console.log('---------------------');
+        console.log('Note not found');
+        console.log('---------------------');
+    }
+
+
 }else if(command === 'remove'){
+    console.log('---------------------');
     console.log('Removing note');
-    notes.removeNote(argv.title);
+    console.log('---------------------');
+
+    var noteRemoved = notes.removeNote(argv.title);
+    console.log('---------------------');
+    var message = noteRemoved ? 'Note was removed' : 'Note not found';    
+    console.log(message);
+    console.log('---------------------');
+
+
 }else{
+    console.log('---------------------');
     console.log('Command not recognized');
+    console.log('---------------------');
 }
+
 
 //npm install yargs@4.7.1 --save
 //--save é para por no JSON
--- NodeJS/NodeJS Developer Course/1. Fundamentals/4.restarting-app-with-nodemon/notes.js
+++ NodeJS/NodeJS Developer Course/1. Fundamentals/4.restarting-app-with-nodemon/notes.js
@@ -2,46 +2,123 @@ console.log('Starting notes.js');
 
 const fs = require('fs');
 
-var addNote = (title, body) =>{
-    console.log('Adding note', title, body);
 
-    var notes = [];
+
+
+/*
+    Functions
+*/
+
+var fetchNotes = () => {
+    try{
+        var notesString = fs.readFileSync('notes-data.json');
+        return JSON.parse(notesString);    
+    }catch(e){   
+        return [];
+    }
+};
+
+
+var saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
+
+/*
+    Real application
+*/
+
+var addNote = (title, body) =>{
+    console.log('---------------------');
+    console.log('Adding note', title, body);
+    console.log('---------------------');
+
+    var notes = fetchNotes();
     var note = {
         title,
         body
     };
 
-    try{
-        var notesString = fs.readFileSync('notes-data.json');
-        notes = JSON.parse(notesString);    
-    }catch(e){   
 
-    }
-
+    //Algum nome duplicado
     var duplicateNotes = notes.filter((note) => note.title === title);
 
+    //Se for === 0, não tem duplicado!
     if(duplicateNotes.length === 0){
         notes.push(note);
-        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+        saveNotes(notes);
+        return note;
     }  
+
+    //else, retorna undefined!
+
 };
+
+
 
 
 var getAll = () => {
+    console.log('---------------------');
     console.log('Getting all notes');
+    console.log('---------------------');
+
+
 };
+
+
+
 
 var getNotes = (title) => {
+    console.log('---------------------');
     console.log('Getting note', title);
+    console.log('---------------------');
+
+    var notes = fetchNotes();
+    var filteredNotes = notes.filter((note) => note. title === title);
+    return filteredNotes[0];
 };
 
+
+
+
 var removeNote = (title) => {
+    console.log('---------------------');
     console.log('Removing note', title);
+    console.log('---------------------');
+
+    //fetch notes.
+    var notes = fetchNotes();
+    //all notes in variable notes
+
+    //filter notes, removing the one with title of argument.
+    var filteredNotes = notes.filter((note) => note.title !== title);
+
+    //save new notes array.
+    saveNotes(filteredNotes);
+
+    //São iguais? Quer dizer que não foi removido nada.
+    return notes.length !== filteredNotes.length;
+
 }
+
+
+
+var logNote = (note) => {
+    console.log('---------------------');
+    console.log(`Title: ${note.title}`);
+    console.log(`Body: ${note.body}`);
+    console.log('---------------------');
+};
+
+
+
+
+
 
 module.exports = {
     addNote,
     getAll,
     getNotes,
-    removeNote
+    removeNote,
+    logNote
 };
