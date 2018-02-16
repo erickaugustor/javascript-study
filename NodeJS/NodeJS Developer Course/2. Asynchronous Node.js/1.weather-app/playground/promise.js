ea02d31b0699605e2f88aeca199db9f73f20421c
erickaugustor <erick.augustoramos@gmail.com>
2018-02-16 16:33:55 -0200

    Advanced Promises - NodeJS

--- NodeJS/NodeJS Developer Course/2. Asynchronous Node.js/1.weather-app/playground/promise.js
+++ NodeJS/NodeJS Developer Course/2. Asynchronous Node.js/1.weather-app/playground/promise.js
@@ -1,14 +1,41 @@
+var asyncAdd = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() =>{
+            if(typeof a === 'number' && typeof b === 'number'){
+                resolve(a + b);
+            }else{
+                reject('Arguments must be numbers!');
+            }
+        }, 1500)
+    });
+};
 
-var somePromise = new Promise((resolve, reject) => {
-    //async code
-    setTimeout(() => {
-        resolve('Hey. It worked!');
-        //reject('Unacle to fulfill promise');
-    }, 2500);
-});
+asyncAdd(1, 2).then((resolve)=>{
+    console.log('Result: ', resolve);
+    return asyncAdd(resolve, 33);
 
-somePromise.then((message) => {
-    console.log('Sucess: ', message);
 }, (errorMessage) => {
-    console.log('Error: ', errorMessage);
+    console.log(errorMessage);
+
+}).then((resolve) => {
+    console.log('Should be 36', resolve);
+
+}).catch((errorMessage) => {
+    console.log(errorMessage);
 });
+
+
+
+// var somePromise = new Promise((resolve, reject) => {
+//     //async code
+//     setTimeout(() => {
+//         resolve('Hey. It worked!');
+//         //reject('Unacle to fulfill promise');
+//     }, 2500);
+// });
+
+// somePromise.then((message) => {
+//     console.log('Sucess: ', message);
+// }, (errorMessage) => {
+//     console.log('Error: ', errorMessage);
+// });
