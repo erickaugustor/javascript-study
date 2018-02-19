2d5b84e0e6034642bf086e140425971c576f5810
erickaugustor <erick.augustoramos@gmail.com>
2018-02-19 13:24:20 -0300

    Weather App

--- NodeJS/NodeJS Developer Course/2. Asynchronous Node.js/1.weather-app/app-promise.js
+++ NodeJS/NodeJS Developer Course/2. Asynchronous Node.js/1.weather-app/app-promise.js
@@ -18,4 +18,33 @@ const argv = yargs
 var encodedAddress = encodeURIComponent(argv.address);
 var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
 
-axios.get
+axios.get(geocodeUrl).then((response) =>{
+    if(response.data.status === 'ZERO_RESULTS'){
+        throw new Error('Unable to find that address.');
+    }
+    
+    var lat = response.data.results[0].geometry.location.lat;
+    var lng = response.data.results[0].geometry.location.lng;
+
+    var weatherUrl = `https://api.darksky.net/forecast/e73bd52c2abe818763478e2bf908c58b/${lat},${lng}`;
+    
+    console.log(response.data.results[0].formatted_address);
+
+    //console.log(response.data);
+
+    return axios.get(weatherUrl);
+
+}).then((response) => {
+
+    var temperature = response.data.currently.temperature;
+    var apparentTemperature = response.data.currently.apparentTemperature;
+    console.log(`It's currently ${temperature}. It feels like ${apparentTemperature}.`);
+
+}).catch((e) => {
+    if(e.code === 'ENOTFOUND'){
+        //console.log(e);
+        console.log('Unable to connect to API servers.');
+    }else{
+        console.log(e.message);
+    }
+});
