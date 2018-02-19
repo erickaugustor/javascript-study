0a703d7f9ff010e91f678b9129b594227bb48049
erickaugustor <erick.augustoramos@gmail.com>
2018-02-19 15:31:26 -0300

    Middleware

--- NodeJS/NodeJS Developer Course/3. Web Server/node-web-server/server.js
+++ NodeJS/NodeJS Developer Course/3. Web Server/node-web-server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
+
 
 var app = express();
 
@@ -8,7 +10,34 @@ var app = express();
 hbs.registerPartials(__dirname + '/views/partials');
 
 app.set('view engine', 'hbs');
+
+
+//Middleware, adicionar funcionalidades!
+app.use((request, response, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${request.method} ${request.url}`;
+
+    console.log(log);
+    //o terceiro parametro é um callback, mas não é necessário.
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err){
+            console('Unable to append to server.log');
+        }
+    });
+    next(); 
+});
+
+
+
+//Middleware: Para de executar as coisas. 
+/* app.use((request, response, next) =>{
+    response.render('maintenance.hbs');
+}) */
+
+//Middleware:
 app.use(express.static(__dirname + '/public'));
+
+
 
 //registrando uma funcionalidade para por no template engine
 hbs.registerHelper('getCurrentYear', () => {
@@ -35,7 +64,7 @@ app.get('/', (request, response) => {
     response.render('home.hbs', {
         pageTitle: 'Home Page', 
         welcomeMessage: 'Welcome to my website',
-    })
+    });
 });
 
 app.get('/about', (request, response) => {
