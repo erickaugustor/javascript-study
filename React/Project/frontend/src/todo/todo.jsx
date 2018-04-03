b8d9acb3e9cfb5f425fc89005476985d7f5e622b
erickaugustor <erick.augustoramos@gmail.com>
2018-04-03 17:42:25 -0300

    Projeto

--- React/Project/frontend/src/todo/todo.jsx
+++ React/Project/frontend/src/todo/todo.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
+
+
+const URL = 'http://localhost:3003/api/todos'
 
 export default class Todo extends Component {
 
   constructor(props){
     super(props)
-    this.state = { description: 'Olar', list: [] }
+    this.state = { description: '', list: [] }
 
     this.handleChange = this.handleChange.bind(this)
     this.handleAdd = this.handleAdd.bind(this)
@@ -18,8 +23,12 @@ export default class Todo extends Component {
   }
 
   handleAdd() {
-    console.log(this.state.description)
+    // console.log(this.state.description)
     // console.log(this)
+
+    const description = this.state.description
+    axios.post(URL, { description })
+          .then(resp => console.log('Funcionou!'))
   }
 
   render() {
